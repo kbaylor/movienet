b5557e03500c8cfab4d568248bdc435cdb9f100f
Kyle Baylor <kyle221b@gmail.com>
2013-04-16 11:41:23 -0400

    Ran the startapp command to create movieapp directory structure. Wrote models in models.py file

--- MovieNet/MovieNet/settings.py
+++ MovieNet/MovieNet/settings.py
@@ -16,7 +16,7 @@ DATABASES = {
         # The following settings are not used with sqlite3:
         'USER': 'root',
         'PASSWORD': 'baylork09',
-        'HOST': 'localhost',                      # Empty for localhost through domain sockets or '127.0.0.1' for localhost through TCP.
+        'HOST': '127.0.0.1',                      # Empty for localhost through domain sockets or '127.0.0.1' for localhost through TCP.
         'PORT': '3306',                      # Set to empty string for default.
     }
 }
@@ -29,7 +29,7 @@ ALLOWED_HOSTS = []
 # http://en.wikipedia.org/wiki/List_of_tz_zones_by_name
 # although not all choices may be available on all operating systems.
 # In a Windows environment this must be set to your system time zone.
-TIME_ZONE = 'America/Chicago'
+TIME_ZONE = 'America/New_York'
 
 # Language code for this installation. All choices can be found here:
 # http://www.i18nguy.com/unicode/language-identifiers.html
@@ -120,6 +120,7 @@ INSTALLED_APPS = (
     'django.contrib.sites',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'movieapp',
     # Uncomment the next line to enable the admin:
     # 'django.contrib.admin',
     # Uncomment the next line to enable admin documentation:
---
+++ MovieNet/movieapp/models.py
@@ -0,0 +1,74 @@
+from django.db import models
+from django.core.validators import MaxValueValidator, MinValueValidator
+
+# Create your models here.
+class Movie(models.Model):
+    id = models.IntegerField(primary_key=True)
+    title = models.CharField(max_length=1024)
+    year = models.IntegerField()
+
+class MovieGenre(models.Model):
+    movie = models.ForeignKey(Movie, primary_key=True)
+    genre = models.CharField(max_length=1024)
+
+class Award(models.Model):
+    name = models.CharField(max_length=255, primary_key=True)
+    year = models.IntegerField()
+    actorAwards = models.ManyToManyField(Movie, through='ActorNomination', related_name='actor_award_set')
+    directorAwards = models.ManyToManyField(Movie, through='DirectorNomination', related_name='director_award_set')
+    class Meta:
+        unique_together = ('name', 'year')
+    
+class Actor(models.Model):
+    id = models.IntegerField(primary_key=True)
+    name = models.CharField(max_length=1024)
+    awards = models.ManyToManyField(Award, through='ActorNomination')
+    movies = models.ManyToManyField(Movie)
+
+class Director(models.Model):
+    id = models.IntegerField(primary_key=True)
+    name = models.CharField(max_length=1024)
+    awards = models.ManyToManyField(Award, through='DirectorNomination')
+    movies = models.ManyToManyField(Movie)
+    
+class MovieNomination(models.Model):
+    movie = models.ForeignKey(Movie, primary_key=True)
+    award = models.ForeignKey(Award)
+    won = models.BooleanField()
+    class Meta:
+        unique_together = ('movie', 'award')
+    
+class ActorNomination(models.Model):
+    movie = models.ForeignKey(Movie, primary_key=True)
+    actor = models.ForeignKey(Actor)
+    award = models.ForeignKey(Award)
+    won = models.BooleanField()
+    class Meta:
+        unique_together = (('movie', 'actor'), ('actor', 'award'))
+    
+class DirectorNomination(models.Model):
+    movie = models.ForeignKey(Movie, primary_key=True)
+    director = models.ForeignKey(Director)
+    award = models.ForeignKey(Award)
+    won = models.BooleanField()
+    class Meta:
+        unique_together = (('movie', 'director'), ('director', 'award'))
+    
+class Member(models.Model):
+    SEX_CHOICES = (('M', 'Male'), ('F', 'Female'))
+    name = models.CharField(max_length=1024)
+    sex = models.CharField(max_length=1, choices=SEX_CHOICES)
+    email = models.EmailField()
+    dob = models.DateField()
+    # in reference to rated relationship
+    ratings = models.ManyToManyField(Movie, through='Rated')
+    
+class Rated(models.Model):
+    member = models.ForeignKey(Member, primary_key=True)
+    movie = models.ForeignKey(Movie)
+    rating = models.IntegerField(validators=[MinValueValidator(0), MaxValueValidator(10)])
+    class Meta:
+        unique_together = ('member', 'movie')
+    
+    
+    
